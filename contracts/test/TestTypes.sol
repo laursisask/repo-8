5e7d66399bb4ce8acd3d44f941db19e9b4d95463
Michał Sieczkowski <michal@ethworks.io>
2021-03-11 12:33:00 +0100

    Fix prettier

--- contracts/test/TestTypes.sol
+++ contracts/test/TestTypes.sol
@@ -40,7 +40,11 @@ contract TestTypes {
         finaliseOn = batch.finaliseOn();
     }
 
-    function hashTransferBody(Types.TransferBody memory body) external pure returns (bytes32) {
+    function hashTransferBody(Types.TransferBody memory body)
+        external
+        pure
+        returns (bytes32)
+    {
         return Types.toHash(body);
     }
 }

ee2805b13f7ce66edba2f0d9d14480ccc176e766
Brian Cloutier <github@briancloutier.com>
2022-06-21 14:34:58 -0700

    Increase initial amount of CustomToken

--- contracts/test/CustomToken.sol
+++ contracts/test/CustomToken.sol
@@ -9,6 +9,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  */
 contract CustomToken is ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 1e27);
+        _mint(msg.sender, 1e31);
     }
 }

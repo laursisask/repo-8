f0d2f4b901a40c3aa43379c8df3d04c9a6dbacb4
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-09 09:33:24 +1000

    Loading external WASM

--- octoterra.js
+++ octoterra.js
@@ -1,11 +1,15 @@
 const go = new Go();
 
 function runWasmAdd() {
-    WebAssembly.instantiateStreaming(fetch('add.wasm'),
-        go.importObject).then((result) => {
-        go.run(result.instance);
-        output.innerHTML = add()
-    });
+    fetch("https://github.com/OctopusSolutionsEngineering/OctopusTerraformExport/raw/main/wasm/convert_project.wasm")
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => {
+            WebAssembly.instantiate(arrayBuffer, go.importObject)
+                .then(result => {
+                    go.run(result.instance);
+                    //output.innerHTML = convertProject()
+                });
+        })
 }
 
 test.onclick = runWasmAdd

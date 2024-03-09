b92b3521971200d50f5e4b681553c42e56132930
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-09 12:27:58 +1000

    Added a logging text area

--- octoterra.js
+++ octoterra.js
@@ -1,5 +1,9 @@
 const go = new Go();
 
+function is_empty_array(array) {
+    return typeof array === 'undefined' || array === null || array.length === 0
+}
+
 function runWasmAdd() {
     response.value = "Processing..."
     text.disabled = true
@@ -8,7 +12,7 @@ function runWasmAdd() {
     fetch(" http://localhost:7071/api/query_parse?message=" + encodeURIComponent(text.value))
         .then(response => response.json())
         .then(entities => {
-            logs.value += JSON.stringify(entities) + "\n"
+            logs.value += JSON.stringify(entities, null, 4) + "\n\n"
 
             fetch("https://github.com/OctopusSolutionsEngineering/OctopusTerraformExport/raw/main/wasm/convert_project.wasm")
                 .then(response => response.arrayBuffer())
@@ -21,11 +25,14 @@ function runWasmAdd() {
                                 const url = new URL(tabs[0].url)
                                 const space = tabs[0].url.split("/")[4]
 
-                                const excludeAllProjects = !entities.project_names
-                                const excludeAllTargets = !entities.target_names
-                                const excludeAllRunbooks = !entities.runbook_names
-                                const excludeAllVariableSets = !entities.library_variable_sets
-                                const excludeAllTenants = !entities.tenant_names
+                                logs.value += JSON.stringify(url.origin) + "\n"
+                                logs.value += JSON.stringify(space) + "\n"
+
+                                const excludeAllProjects = is_empty_array(entities.project_names)
+                                const excludeAllTargets = is_empty_array(entities.target_names)
+                                const excludeAllRunbooks = is_empty_array(entities.runbook_names)
+                                const excludeAllVariableSets = is_empty_array(entities.library_variable_sets)
+                                const excludeAllTenants = is_empty_array(entities.tenant_names)
 
                                 convertProject(
                                     url.origin,

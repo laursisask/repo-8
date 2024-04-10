4583712d0065b8865162d4b03e2ef4a0ef3d7df2
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-10 16:42:15 +1000

    Include the word promote

--- octoterra.js
+++ octoterra.js
@@ -952,6 +952,7 @@ function getEnvironmentId(host, spaceId, environmentName) {
 
 function requiresReleaseHistory(query) {
     return (query.toLowerCase().indexOf("deploy") !== -1 ||
+            query.toLowerCase().indexOf("promote") !== -1 ||
             query.toLowerCase().indexOf("release") !== -1) &&
         query.toLowerCase().indexOf("log") === -1
 }
